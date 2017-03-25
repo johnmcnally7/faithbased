08b377d78684a6bb3c3a5950d754778ee69ee3be
johnmcnally7 <johnmcnally7@172-17-16-235.DYNAPOOL.NYU.EDU>
2017-03-25 14:44:02 -0400

    changes made in sat session w/ chris

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //create map
  var map = L.map('mapcontainer', {
-        center: [40.6622, -73.9442],
+        center: [40.6523, -73.9442],
         zoom: 12
     });
 
@@ -10,11 +10,37 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
       }).addTo(map);
 
-        var faithbased = 'https://johnmcnally7.carto.com/api/v2/viz/dc91aede-6867-4919-87bf-76827dd57d88/viz.json';
+    //add faithbased geojson data to map
+    var layer = L.geoJson(faithbased, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker (latlng, style(feature))
+        }
+    }).addTo(map);
 
-        cartodb.createLayer(map, faithbased)
-        .addTo(map)
-        .on('done', function(layer)){}
+    //color selector for pop density
+    function getColor(d) {
+         return d > 100000 ? '#084594' :
+                d > 50000  ? '#2171b5' :
+                d > 25000  ? '#4292c6' :
+                d > 10000  ? '#6baed6' :
+                d > 1000   ? '#9ecae1' :
+                d > 1      ? '#c6dbef' :
+                             '#eff3ff' ;
+    }
+
+    //fill color for geojson based on the feature
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.faithbas18),
+            weight: 1,
+            opacity: .9,
+            color: 'white',
+            dashArray: '2',
+            fillOpacity: 0.7,
+            radius: 4
+        };
+    }
+
 
  //scale bar 
 L.control.scale({
